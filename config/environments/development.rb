ece41dddb605b7878968b0eaca46256cbba7ddd2
sreeram sama <sama.sreeram13@gmail.com>
2016-12-15 14:32:43 -0800

    change cache store in dev-mode to memcachier

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
     ActiveSupport::Cache::Store.logger = Rails.logger
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :dalli_store
     config.public_file_server.headers = {
       'Cache-Control' => 'public, max-age=172800'
     }
